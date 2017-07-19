f905c88c4156580b1aea081b6f621104c7b712df
Andrew Gopienko <a.gopienko@ndmsystems.com>
2017-07-19 09:55:44 +0700

    parallel bench

--- bench_test.go
+++ bench_test.go
@@ -23,14 +23,20 @@ func BenchmarkStartRegisteredServer(b *testing.B) {
 func BenchmarkCallPid(b *testing.B) {
 	pid, err := run_server()
 	if err == nil {
+
 		req := &reqInc{}
-		for i := 0; i < b.N; i++ {
-			pid.Call(req)
-		}
+		b.ResetTimer()
+
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				pid.Call(req)
+			}
+		})
 	}
 }
 
 func BenchmarkCallName(b *testing.B) {
+
 	prefix := "gsGroup"
 	name := "test_proc_123"
 	opts := &Opts{
@@ -44,20 +50,28 @@ func BenchmarkCallName(b *testing.B) {
 	req := &reqInc{}
 
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		pid = WhereisPrefix(prefix, name)
-		if pid == nil {
-			b.Fatal("pid == nil")
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			pid = WhereisPrefix(prefix, name)
+			if pid == nil {
+				b.Fatal("pid == nil")
+			}
+			pid.Call(req)
 		}
-		pid.Call(req)
-	}
+	})
 }
 
 func BenchmarkCastPid(b *testing.B) {
 	pid, err := run_server()
 	if err == nil {
-		for i := 0; i < b.N; i++ {
-			pid.Cast(cmdTest)
-		}
+
+		b.ResetTimer()
+
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				pid.Cast(cmdTest)
+			}
+		})
 	}
 }
